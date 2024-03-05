94078c1d3c03192e2d86c26b4e8c0d7a40adb5dd
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-03-05 09:29:06 +0530

    Run crawler and log page vists

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello world");
+const { argv } = require("node:process");
+const { crawlPage } = require("./crawl");
+
+async function main() {
+  if (argv.length !== 3) {
+    console.error("Not right usage of Crawlr. Please provide only one argument");
+    process.exit(1);
+  }
+
+  const baseURL = argv[argv.length - 1];
+  const pages = await crawlPage(baseURL, baseURL, {});
+  console.log("These are all pages: ", pages);
+}
+
+main();
